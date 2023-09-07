e0646303d60b9874c1b33825118cae0e9df86070
Friday Joshua <fridayjoshua952@gmail.com>
2023-09-07 05:26:56 -0700

    custom event added

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const btns = Array.from(document.getElementsByTagName("button"));
 btns.forEach(function(btn){
     btn.addEventListener("click",function(evt){
         alert('You have added 1 item to your cart');
+        gtag("event","add_to_cart"{
+            "event_category":"conversion",
+            "event_label":"One item has been added to cart"
+        });
     })
 });
 
