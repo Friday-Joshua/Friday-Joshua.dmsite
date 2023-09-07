fa1e6128e4c1719b38ae4d624fbaed41b882f6a2
Friday Joshua <fridayjoshua952@gmail.com>
2023-09-07 05:29:55 -0700

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const btns = Array.from(document.getElementsByTagName("button"));
 btns.forEach(function(btn){
     btn.addEventListener("click",function(evt){
         alert('You have added 1 item to your cart');
-        gtag("event","add_to_cart"{
+        gtag("event","add_to_cart",{
             "event_category":"conversion",
             "event_label":"One item has been added to cart"
         });
